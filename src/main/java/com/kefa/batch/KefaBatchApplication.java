4e2ea8b52a6912466b2ec0f0dd19e7d2026bba4b
malslapq <malslapq@gmail.com>
2025-04-11 16:49:27 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/kefa/batch/KefaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kefa.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KefaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KefaBatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kefa/batch/infrastructure/batch/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.kefa.batch.infrastructure.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+}
